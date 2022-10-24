4f107af3244b97ca19ed5a0ba1acc2aaa47bae93
Tetiana Niegrieieva <tanyaenlang9898@gmail.com>
2022-10-24 13:42:59 +0300

    Server created. Package json and eslint configuration added.

---
+++ lecture2/index.js
@@ -0,0 +1,17 @@
+const http = require('http');
+const ip = require('ip');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+console.log(ip.address());
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World');
+});
+
+server.listen(port, hostname, () =>{
+    console.log(`Server is running at ${port}:${hostname}/`);
+});
